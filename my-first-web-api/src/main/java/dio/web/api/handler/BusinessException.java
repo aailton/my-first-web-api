55f01e115ec7bc498ab90c1d6bf158f4b38a91ac
Ailton Alves <Ailton Alves@DESKTOP-F4A6D0S>
2024-03-08 22:27:19 -0300

    commit initial

---
+++ my-first-web-api/src/main/java/dio/web/api/handler/BusinessException.java
@@ -0,0 +1,16 @@
+package dio.web.api.handler;
+
+public class BusinessException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BusinessException(String mensagem) {
+		super(mensagem);
+	}
+
+	public BusinessException(String mensagem, Object ... params) {
+		super(String.format(mensagem, params));
+	}
+}
